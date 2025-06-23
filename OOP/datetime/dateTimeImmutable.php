6a5ef81ca8b6ea4f8d6524e92b143ef066c579ce
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2025-06-23 20:01:02 +0700

    learn sub, diff, modify datetime

--- OOP/datetime/dateTimeImmutable.php
+++ OOP/datetime/dateTimeImmutable.php
@@ -1,21 +1,47 @@
 <?php
 
-$dateImmutable = new DateTimeImmutable(); // waktu saat ini (default timezone)
+$dateImmutable = new DateTimeImmutable(); // current time6(default timezone)
 
-// Konversi ke string
-$str = $dateImmutable->format("Y-m-d H:i:s");
+echo "Now : " . $dateImmutable->format("d-F-Y H:i:s") . PHP_EOL;
 
-// Buat ulang dengan timezone Asia/Jakarta
+// Subtracts an amount of date
+$new_date = $dateImmutable->sub(new DateInterval("P5Y2M4D"));
+
+try {
+
+    // ability to modify date with string using modify()
+    $modify_new_date = $new_date->modify("+1 month");
+
+    echo "+1 month with modify : " . $modify_new_date->format("d-F-Y") . PHP_EOL;
+
+  // If an invalid Date/Time string is passed, DateMalformedStringException is thrown. 
+} catch (DateMalformedStringException | Exception $err) {
+    echo "date error : {$err->getMessage()} " . PHP_EOL; 
+}
+
+//diff is equals to ($new_date - $dateImmutable)
+$interval = $dateImmutable->diff($new_date);
+
+// convert date to string
+$str = $new_date->format("Y-m-d H:i:s");
+
+// %a will output the total number of days.
+echo "Day difference: " .  $interval->format('%a total days')."\n";
+
+// While %d will only output the number of days not already covered by the
+// month.
+echo "year,month,days difference: " . $interval->format('%y year, %m month, %d days')."\n";
+
 $newDate = DateTimeImmutable::createFromFormat(
     "Y-m-d H:i:s",
     $str,
     new DateTimeZone("Asia/Jakarta")
 );
 
-// Cek apakah parsing berhasil
 if ($newDate) {
     echo $newDate->format("Y-m-d H:i:s T") . PHP_EOL;
 } else {
     echo "Parsing gagal" . PHP_EOL;
 }
 ?>
+
